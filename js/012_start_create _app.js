eae256bbc15b54effb7aa87f60e8dc3e6769caa1
yankovski88 <boets003@mail.ru>
2020-07-19 13:43:10 +0300

    first commit udemy js 012 lesson

---
+++ js/012_start_create _app.js
@@ -0,0 +1,22 @@
+let numberOfFilms = +prompt("How many did you watch film?", "");
+let question1 = prompt("What did you watch last film?", "");
+let question2 = prompt("What will you give ball?", "");
+
+let question3 = prompt("What did you watch last film?", "");
+let question4 = prompt("What will you give ball?", "");
+// let question1 = "son";
+// let question2 = "7";
+//
+// let numberOfFilms = "blade"
+let personalMovieDB = {
+    count: numberOfFilms,
+    movies: {
+    },
+    actors: {},
+    genres: [],
+    privat: false
+};
+personalMovieDB.movies[question1] = question2;
+personalMovieDB.movies.question3 = question4; // через . плохо работает, если написать ответ где много пробелов и т.д.
+console.log(personalMovieDB);
+// console.log(personalMovieDB.count);
